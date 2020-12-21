b2849a93c750849ee294a447bc17cb1523f31f0e
rgallov <rgallov@gmail.com>
2020-12-21 14:59:32 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import vuetify from "./plugins/vuetify";
 import axios from 'axios';
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = 'http://localhost:3000/api/';
-// axios.defaults.baseURL = 'https://serene-wave-60007.herokuapp.com/api/';
+// axios.defaults.baseURL = 'http://localhost:3000/api/';
+axios.defaults.baseURL = 'https://serene-wave-60007.herokuapp.com/api/';
 new Vue({
     router,
     store,
